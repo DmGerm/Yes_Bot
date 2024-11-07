54dc39f2f1711a0fc91e29529200ea90129e5495
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-11-07 22:45:02 +0300

    this commit will write base bot structure

--- DNS_YES_BOT/Program.cs
+++ DNS_YES_BOT/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("7604420018:AAH2SlKhFP78Neg0F86qGV1ARk_19dzRpac", cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnError += OnError;
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); 
+
+async Task OnError(Exception exception, HandleErrorSource source)
+{
+    Console.WriteLine(exception); 
+}
+
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text == "/start")
+    {
+        await bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
+            replyMarkup: new InlineKeyboardMarkup().AddButtons("Left", "Right"));
+    }
+}
+
+
+async Task OnUpdate(Update update)
+{
+    if (update is { CallbackQuery: { } query }) 
+    {
+        await bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
+        await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
+    }
+}

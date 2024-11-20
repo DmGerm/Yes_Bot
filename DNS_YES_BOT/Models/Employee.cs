fba17b096e36cc77ff91a7fb71ccdff40d8fecf2
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-11-20 22:22:23 +0300

    thic commit will adding models

---
+++ DNS_YES_BOT/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace DNS_YES_BOT.Models
+{
+    internal class Employee
+    {
+        public Guid EmployeeId;
+        public string FirstName = string.Empty;
+        public string LastName = string.Empty;
+    }
+}
---
+++ DNS_YES_BOT/Models/Shop.cs
@@ -0,0 +1,9 @@
+﻿namespace DNS_YES_BOT.Models
+{
+    internal class Shop
+    {
+        public Guid ShopId;
+        public string ShopName = string.Empty;
+        public List<Guid> EmployeesId = [];
+    }
+}

65fcdc5d3c1dc46a3e8b228f3ab485983e0f7f23
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-11-26 22:46:27 +0300

    this commit will repair code

--- DNS_YES_BOT/EventHandlers/OnMessageHandler.cs
+++ DNS_YES_BOT/EventHandlers/OnMessageHandler.cs
@@ -16,23 +16,28 @@ namespace DNS_YES_BOT.EventHandlers
             }
             var command = msg.Text.Split('@')[0];
 
-            if (msg.Text == "/start")
+            if (command == "/start")
             {
                 await _botClient.SendMessage(msg.Chat, "Выберите ваш филиал, чтобы подтвердить прочтение информации!",
                     replyMarkup: new InlineKeyboardMarkup().AddButtons("Выборг Советская", "Тихвин"));
             }
 
-            if (msg.Text == "/admin_service")
+            if (command == "/admin_service")
             {
                 await _botClient.SendMessage(msg.Chat.Id, "hi");
-                await _botClient.SendMessage(msg.Chat.Id,
-                    "Выберите действие:",
-                    replyMarkup: new InlineKeyboardMarkup(
-                    [
-                    [InlineKeyboardButton.WithCallbackData("Добавить администратора", "admin_add")],
-                    [InlineKeyboardButton.WithCallbackData("Добавить магазин", "shop_add")],
-                    [InlineKeyboardButton.WithCallbackData("Добавить сотрудника", "employee_add")]
-                    ]));
+                await _botClient.SendMessage(
+               msg.Chat.Id,
+               "Выберите действие:",
+               replyMarkup: new InlineKeyboardMarkup(
+               [
+                [
+                    InlineKeyboardButton.WithCallbackData("Добавить администратора", "admin_add"),
+                    InlineKeyboardButton.WithCallbackData("Добавить сотрудника", "employee_add")
+                ],
+                [
+                    InlineKeyboardButton.WithCallbackData("Добавить магазин", "shop_add")
+                ]
+               ]));
 
             }
         }

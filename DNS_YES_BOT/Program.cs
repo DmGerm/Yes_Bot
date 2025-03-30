9767a25d599583ce8e8975a625c1f5887450716e
Dmitriy Germanov <seltis.gtn@gmail.com>
2025-03-30 21:51:33 +0300

    this commit will repair docker files

--- DNS_YES_BOT/Program.cs
+++ DNS_YES_BOT/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DNS_YES_BOT.BotService;
 using Microsoft.Extensions.Configuration;
+using System.Text.RegularExpressions;
 
 internal class Program
 {
@@ -10,9 +11,17 @@ internal class Program
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
         IConfiguration configuration = builder.Build();
 
+        var stringFromEnv = File.ReadAllText("token.env");
+        string pattern = @"^(?:\w+)=([\w:]+)$";
+        if (!Regex.IsMatch(stringFromEnv, pattern))
+        {
+            throw new InvalidOperationException("Invalid token.env file format.");
+        }
+        var token = Regex.Match(stringFromEnv, pattern).Groups[1].Value;
+        Console.WriteLine(token);
         var botToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")
-                      ?? configuration["Telegram:BotToken"]
-                      ?? throw new InvalidOperationException("Bot token is not provided!");
+                                        ?? token
+                                        ?? throw new InvalidOperationException("Bot token is not provided!");
 
         BotService botService = new(botToken);
         await botService.BotRun();
